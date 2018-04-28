531082a9c451b2fe0e4e0fee917ebea429ca3928
Charpell <charpellumeh@gmail.com>
2018-04-28 22:13:44 +0100

    Logging iut the User

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,6 +10,12 @@ module.exports = (app) => {
   
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  // Passport automatically logs the user out on the request mathod
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
