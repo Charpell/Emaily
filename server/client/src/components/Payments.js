5ecac78b1b316854788e54d186f9f10ab5543771
Charpell <charpellumeh@gmail.com>
2018-04-29 23:56:47 +0100

    - Integrating Stripe Tokens - Fix Payment Bugs and Styling

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -4,15 +4,19 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
   render() {
-    debugger;
-    
     return (
       <div>
-        <StripeCheckout 
+        <StripeCheckout
+          name="Emaily"
+          description="$5 for 5 email credits" 
           amount={500}
           token={token => console.log(token)}
           stripeKey={process.env.REACT_APP_STRIPE_KEY}
-        />
+        >
+          <button className="btn">
+            Add Credits
+          </button>
+        </StripeCheckout>
       </div>
     );
   }
