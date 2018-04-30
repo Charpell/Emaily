001816d6ff96ff08c76b94ebefa6ccf2051b8242
Charpell <charpellumeh@gmail.com>
2018-04-30 21:28:59 +0100

    - BoilerPlate for Sending Emails

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -13,6 +13,24 @@ class Mailer extends helper.Mail {
     this.subject = subject;
     this.body = new helper.Content('text/html', content);
     this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackSettings);
   }
 
 }
