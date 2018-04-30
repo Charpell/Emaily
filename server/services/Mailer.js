bd5cafd09e423e222c455af0fe0c347b12d7f088
Charpell <charpellumeh@gmail.com>
2018-04-30 21:17:05 +0100

    - Creating the Mailer Constructor

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -6,7 +6,15 @@ const helper = sendgrid.mail;
 
 
 class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content ) {
+    super();
 
-};
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+
+}
 
 module.exports = Mailer;
