4bc9b561b2c0839fdae769795cd711d1f51665b8
Charpell <charpellumeh@gmail.com>
2018-05-01 18:48:47 +0100

    Creating back button for the Form Review

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,15 @@
+// SurveyFormReview shows users their form inputs for review
+
 import React from 'react'
 
-const SurveyReview = (props) => {
+const SurveyReview = ({ onCancel }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+        Back
+      </button>
     </div>
   )
 };
